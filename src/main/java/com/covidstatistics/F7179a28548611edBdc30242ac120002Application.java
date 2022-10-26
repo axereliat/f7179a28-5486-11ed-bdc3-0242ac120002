1bee3a940608637e37309c29d400f1af4c6077c2
axereliat <axereliat@gmail.com>
2022-10-26 10:53:25 +0300

    Initial commit

---
+++ src/main/java/com/covidstatistics/F7179a28548611edBdc30242ac120002Application.java
@@ -0,0 +1,13 @@
+package com.covidstatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F7179a28548611edBdc30242ac120002Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(F7179a28548611edBdc30242ac120002Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/covidstatistics/F7179a28548611edBdc30242ac120002ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covidstatistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F7179a28548611edBdc30242ac120002ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
